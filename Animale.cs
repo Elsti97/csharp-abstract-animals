a3493e6576641dedeac64e82af3fea30743b5cf5
Elsti97 <elsti.shehati@hotmail.it>
2023-03-23 14:19:54 +0100

    Exercise 1 done

---
+++ Animale.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzz");
+        }
+
+        public abstract void Verso();
+        public abstract void Mangia();
+    }
+
+    public class Cane : Animale 
+    { 
+        public override void Verso()
+        {
+            Console.WriteLine("Bau bau");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia carne");
+        }
+    }
+
+    public class Passerotto : Animale 
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Pio pio");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia erba");
+        }
+    }
+
+    public class Aquila : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("AAAAAAH");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia vermi");
+        }
+    }
+
+    public class Delfino : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Fischio");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia pesci");
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_abstract_animals;
+
+Console.WriteLine("Cane: ");
+Cane cane = new Cane();
+cane.Verso();
+cane.Dormi();
+cane.Mangia();
+
+Console.WriteLine("Passerotto: ");
+Passerotto passerotto = new Passerotto();
+passerotto.Verso();
+passerotto.Dormi();
+passerotto.Mangia();
+
+Console.WriteLine("Aquila: ");
+Aquila aquila = new Aquila();
+aquila.Verso();
+aquila.Dormi();
+aquila.Mangia();
+
+Console.WriteLine("Delfino: ");
+Delfino delfino = new Delfino();
+delfino.Verso();
+delfino.Dormi();
+delfino.Mangia();
